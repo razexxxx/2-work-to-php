6eaf41dc38133a6242f01f30c027183194df823a
razexxxx <bsziro123@gmail.com>
2025-02-18 16:52:51 +0300

    1 commit

---
+++ 2 дз 3 задание.php
@@ -0,0 +1,15 @@
+<?php
+function getDaysInMonth($month) {
+    $daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if ($month < 1 || $month > 12) {
+        return "Некорректный номер месяца. Введите число от 1 до 12.";
+    }
+
+    return $daysInMonths[$month - 1];
+}
+
+$month = (int)readline("Введите порядковый номер месяца (1-12): ");
+
+echo getDaysInMonth($month) . "\n";
+?>
